cf59a114644c3b121aa990084a74873b493d4b3f
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-11-18 22:21:54 +1100

    Remove watch/serve

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const collections = require('metalsmith-collections');
 const handlebars = require('handlebars');
 const dateFormatter = require('metalsmith-date-formatter');
 const inlineCss = require('metalsmith-inline-css');
-// const serve = require('metalsmith-serve');
-// const watch = require('metalsmith-watch');
 // TODO: RSS feeds
 
 handlebars.registerHelper('links', function(items, options) {
@@ -61,16 +59,6 @@ Metalsmith(__dirname)
     },
   }))
   .use(inlineCss())
-  // .use(serve({
-  //   port: 8081,
-  //   verbose: true
-  // }))
-  // .use(watch({
-  //   paths: {
-  //     '${source}/**/*': true, // ${source} is replaced by metalsmith.source()
-  //     'layout/**/*': true,
-  //   }
-  // }))
   // .use((x, ...rest) => {
   //   console.log(x, rest);
   //   return x;
